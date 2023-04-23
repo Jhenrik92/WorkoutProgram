e92aaf04a55af54fb291322a63b669a0bcad36f5
Jhenrik92 <jhenrik92@gmail.com>
2023-04-23 09:16:44 +0200

    Changes

--- functions.js
+++ functions.js
@@ -1,55 +1,35 @@
 
 
 
-let itemCount = 0;
 
-$('#inputExercise').keypress(function (keyPressEvent) {
-    let key = keyPressEvent.which;
+
+$('button').click(function () {
+    $("#item" + itemCount).replaceWith('<p>' + exercise + '</p>')
+    $("#itemReps" + itemCount).replaceWith('<p>' + setsReps + '</p>')
+    $("#itemKilo" + itemCount).replaceWith('<p>' + exercise + '</p>');
+})
+
+
+$('#test').keypress(function(keypressEvent) {
+    let key = keypressEvent.which;
     if (key == 13) {
-    let exercise = $('#inputExercise').val()
-    if (exercise == '') {
-        return;
-    } else {
-        itemCount++;
-
-        $('#firstRow').append('<tr id="item' + itemCount + '"><td>' + exercise + '</td></tr>');
-        $('#inputExercise').hide();
-        $('#inputRepsSets').css('visibility', 'visible');
-        $('#inputExercise').val('')
+        let test = $('#test').val();
+        if (test == '') {
+            return;
+        } else {
+            $('#testtwo').append('<p>' + test + '</p>');
+        }
     }
-}
-});
+})
 
-$('#inputRepsSets').keypress(function (keyPressEvent) {
-    let key = keyPressEvent.which;
+$('#inputExercise').keypress(function(keypressEvent) {
+    let key = keypressEvent.which;
     if (key == 13) {
-    let RepsSets = $('#inputRepsSets').val()
-    if (RepsSets == '') {
-        return;
-    } else {
-        itemCount++;
-
-        $('#item1').append('<td id="item' + itemCount + '">' + RepsSets + '</td>');
-        $('#inputRepsSets').hide();
-        $('#inputKilograms').css('visibility', 'visible');
-        $('#inputRepsSets').val('')
+        let inputExercise = $('#inputExercise').val();
+        if (test == '') {
+            return;
+        } else {
+            $('#testthree').append('<p>' + inputExercise + '</p>');
+        }
     }
-}
-});
-
-$('#inputKilograms').keypress(function (keyPressEvent) {
-    let key = keyPressEvent.which;
-    if (key == 13) {
-    let Kilograms = $('#inputKilograms').val()
-    if (Kilograms == '') {
-        return;
-    } else {
-        itemCount++;
-
-        $('#item1').append('<td id="item' + itemCount + '">' + Kilograms + '</td>');
-        $('#inputKilograms').hide();
-        $('#inputExercise').show();
-        $('#inputKilograms').val('')
-    }
-}
-});
+})
